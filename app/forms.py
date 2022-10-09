6407c6debe28c3b75566e8eab1d27334e216acc9
KentStuder22 <77794005+KentStuder22@users.noreply.github.com>
2022-10-09 15:46:28 -0500

    working raw html for login

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ from .models import User
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    #remember_me = BooleanField('Remember Me')
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
 
 class RegistrationForm(FlaskForm):
