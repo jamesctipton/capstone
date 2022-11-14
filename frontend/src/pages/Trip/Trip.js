829d65fc8775ac04b81fbeb38fcb15dfa3874e23
james tipton <jctppc@umsystem.edu>
2022-11-13 19:04:19 -0600

    trip page

---
+++ frontend/src/pages/Trip/Trip.js
@@ -0,0 +1,25 @@
+import './Login.css';
+import React, { useState } from 'react';
+import { 
+  createTheme, 
+} from '@mui/material';
+
+const url = 'http://127.0.0.1:5000/trip/:'
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#CF7D30',
+      darker: '#BE6C20'
+    }
+  }
+});
+
+
+const Trip = (props) => {
+
+    return (
+        <div> 
+        </div>
+  )};
+  
+  export default Trip;
