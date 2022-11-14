82af702b01735f5c95212280407b5ba6d6a9961e
Kent Studer <personal@Kents-MacBook-Pro.local>
2022-11-14 11:50:48 -0600

    removing hard coded css

--- frontend/src/pages/Trip/JoinCreate.js
+++ frontend/src/pages/Trip/JoinCreate.js
@@ -31,10 +31,10 @@ const JoinCreate = (isLoggedIn) => {
     const [groupCode, setGroupCode] = useState("")
 
     return (
-        <div style={{ display: 'grid', gridTemplateColumns: '1fr 3px 1fr', marginTop: '20%' }}>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 3px 1fr', marginTop: '10%' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between' }}>
                 <Typography variant='h3' sx={{ color: '#CF7D30', marginBottom: '13%' }}>Join Group</Typography>
-                <Typography variant='h6' sx={{ marginBottom: '6%' }}>Please enter 4 digit group code</Typography>
+                <Typography variant='h6' sx={{ marginBottom: '6%', alignItems: 'center' }}>Please enter the group's 4 digit code</Typography>
                 <TextField
                     variant='outlined'
                     placeholder='Code'
@@ -42,30 +42,30 @@ const JoinCreate = (isLoggedIn) => {
                     onChange={(e) => setGroupCode(e.target.value)}
                     sx={{ width: '65%', paddingBottom: 5 }}
                     InputProps={{
-                        startAdornment: <InputAdornment sx={{ marginLeft: '41%' }}></InputAdornment>
+                        startAdornment: <InputAdornment position="start" sx={{ marginLeft: '42%' }}></InputAdornment>
                     }}
                     required
                 ></TextField>
                 {(groupCode.length == 4) ?
                 <Button 
-                type='submit'
-                variant="outlined"
-                sx={{ m: 1, borderWidth: 3, borderRadius: 30, background: 'rgba(207, 125, 48, 0.31)', fontWeight: 600, whiteSpace: 'nowrap', minWidth: 'maxContent', width: '50%', height: 50, fontSize: 15, marginTop: '10%' }}
-                size="large"
-                component={Link} to={"/trip/" + groupCode}
+                    type='submit'
+                    variant="outlined"
+                    sx={{ m: 1, borderWidth: 3, borderRadius: 30, background: 'rgba(207, 125, 48, 0.31)', fontWeight: 600, whiteSpace: 'nowrap', minWidth: 'maxContent', width: '50%', height: 50, fontSize: 15, marginTop: '10%' }}
+                    size="large"
+                    component={Link} to={"/trip/" + groupCode}
                 >Submit</Button>
                 :
                 <Button 
-                        type='submit'
-                        variant="outlined"
-                        sx={{ m: 1, borderWidth: 3, borderRadius: 30, background: 'rgba(207, 125, 48, 0.31)', fontWeight: 600, whiteSpace: 'nowrap', minWidth: 'maxContent', width: '50%', height: 50, fontSize: 15, marginTop: '10%' }}
-                        size="large"
-                        component={Link} to={"/join-create"}
+                    type='submit'
+                    variant="outlined"
+                    sx={{ m: 1, borderWidth: 3, borderRadius: 30, background: 'rgba(207, 125, 48, 0.31)', fontWeight: 600, whiteSpace: 'nowrap', minWidth: 'maxContent', width: '50%', height: 50, fontSize: 15, marginTop: '10%' }}
+                    size="large"
+                    component={Link} to={"/join-create"}
                 >Submit</Button>
             }
             </div>
                 <Divider orientation='vertical' variant='middle' flexItem sx={{ background: '#CF7D30' }}></Divider>
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Typography variant='h3' sx={{ color: '#CF7D30' }}>Create Group</Typography>
                 <IconButton
                     onClick={() => {}}
@@ -74,11 +74,11 @@ const JoinCreate = (isLoggedIn) => {
                 </IconButton>
                 <TextField
                     variant='outlined'
-                    sx={{ width: '65%', paddingTop: 1.1, paddingBottom: 1 }}
+                    sx={{ width: '65%' }}
                     placeholder='Group Name'
                     helperText="Please enter a name for your group"
                     InputProps={{
-                        startAdornment: <InputAdornment sx={{ marginLeft: '33%' }}></InputAdornment>
+                        startAdornment: <InputAdornment position='start' sx={{ marginLeft: '35.5%' }}></InputAdornment>
                     }}
                     FormHelperTextProps={{
                         style: {
@@ -88,11 +88,11 @@ const JoinCreate = (isLoggedIn) => {
                     }}
                     required
                 ></TextField>
-                <FormGroup style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 100, paddingLeft: 25, paddingBottom: 5 }}>
+                <FormGroup style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', gap: 100, paddingLeft: 25, paddingBottom: 5 }}>
                     <Typography variant='h7' >*Private or Public group</Typography>
                     <FormControlLabel control={<Switch defaultChecked color='warning' />} />
                 </FormGroup>
-                <Typography variant='p' style={{ textAlign: 'center', fontSize: 'small', paddingLeft: 10, paddingRight: 10, paddingBottom: 40 }}>Public means everyone in the group can invite others, Private means only selected persons can</Typography>
+                <Typography variant='p' style={{ textAlign: 'center', fontSize: 'small', paddingLeft: 10, paddingRight: 10 }}>Public means everyone in the group can invite others, Private means only selected persons can</Typography>
                 <Button 
                     type='submit'
                     variant="outlined"
