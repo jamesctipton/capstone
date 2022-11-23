37ab4298dd1be0f200c9c3e03565ed78a93bb2de
Kent Studer <personal@Kents-MacBook-Pro.local>
2022-11-23 12:39:37 -0600

    changing wording for desination

--- frontend/src/pages/Search/Search.js
+++ frontend/src/pages/Search/Search.js
@@ -17,33 +17,33 @@ const theme = createTheme({
 const Search = () => {
 
     const [isActive, setActive] = useState({
-        places: true,
+        desination: true,
         hotels: false,
         flights: false,
         poi: false
     })
 
-    const [initCriteria, setInitCriteria] = useState("places")
+    const [initCriteria, setInitCriteria] = useState("desination")
 
-    const setPlacesActive = () => {
-        isActive.places = true
+    const setDesinationActive = () => {
+        isActive.desination = true
         isActive.hotels = false
         isActive.flights = false
         isActive.poi = false
-        setActive({...isActive, places: true})
+        setActive({...isActive, desination: true})
         setActive({...isActive, hotels: false})
         setActive({...isActive, flights: false})
         setActive({...isActive, poi: false})
 
-        setInitCriteria("places")
+        setInitCriteria("desination")
     }
 
     const setHotelsActive = () => {
-        isActive.places = false
+        isActive.desination = false
         isActive.hotels = true
         isActive.flights = false
         isActive.poi = false
-        setActive({...isActive, places: false})
+        setActive({...isActive, desination: false})
         setActive({...isActive, hotels: true})
         setActive({...isActive, flights: false})
         setActive({...isActive, poi: false})
@@ -52,11 +52,11 @@ const Search = () => {
     }
 
     const setFlightsActive = () => {
-        isActive.places = false
+        isActive.desination = false
         isActive.hotels = false
         isActive.flights = true
         isActive.poi = false
-        setActive({...isActive, places: false})
+        setActive({...isActive, desination: false})
         setActive({...isActive, hotels: false})
         setActive({...isActive, flights: true})
         setActive({...isActive, poi: false})
@@ -65,11 +65,11 @@ const Search = () => {
     }
 
     const setPOIActive = () => {
-        isActive.places = false
+        isActive.desination = false
         isActive.hotels = false
         isActive.flights = false
         isActive.poi = true
-        setActive({...isActive, places: false})
+        setActive({...isActive, desination: false})
         setActive({...isActive, hotels: false})
         setActive({...isActive, flights: false})
         setActive({...isActive, poi: true})
@@ -81,10 +81,10 @@ const Search = () => {
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '5%' }}>
             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', width: '100%', marginBottom: '1%'}}>
                 <Button  
-                    sx={{ backgroundColor: isActive.places ? 'rgba(207, 125, 43, .31)' : 'rgba(0,0,0,0)', marginLeft: 2, marginRight: 2 }} 
-                    onClick={() => setPlacesActive()}
+                    sx={{ backgroundColor: isActive.desination ? 'rgba(207, 125, 43, .31)' : 'rgba(0,0,0,0)', marginLeft: 2, marginRight: 2 }} 
+                    onClick={() => setDesinationActive()}
                     fullWidth
-                >Places</Button>
+                >Desination</Button>
                 <Divider orientation="vertical" variant="middle" flexItem sx={{ background: '#CF7D30', width: 1.5}}></Divider>
                 <Button  
                     sx={{ backgroundColor: isActive.hotels ? 'rgba(207, 125, 43, .31)' : 'rgba(0,0,0,0)', marginLeft: 2, marginRight: 2 }} 
@@ -105,7 +105,7 @@ const Search = () => {
                 >Points of Interest</Button>
             </div>
             <Divider orientation="horizontal" variant="fullwidth" flexItem sx={{ background: '#CF7D30'}}></Divider>
-            {initCriteria === 'places' 
+            {initCriteria === 'desination' 
                 ? <div style={{width: '80%', marginTop: '2%'}}>
                     <FormControl fullWidth>
                         <OutlinedInput placeholder="Search Countries, Cities or Specific Locations" variant="outlined" sx={{borderWidth: 3, borderRadius: 30, whiteSpace: 'nowrap', minWidth: 334}} fullWidth
