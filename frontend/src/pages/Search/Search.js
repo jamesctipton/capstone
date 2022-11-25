86e106e3228befca98bb5822445414e03b4dbae3
james tipton <jctppc@umsystem.edu>
2022-11-24 21:20:16 -0600

    datagrid

--- frontend/src/pages/Search/Search.js
+++ frontend/src/pages/Search/Search.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Divider, FormControl, OutlinedInput, IconButton, createTheme, Box, Typography, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import axios from "axios";
+import { DataGrid } from '@mui/x-data-grid';
 
 const theme = createTheme({
     palette: {
@@ -116,10 +117,54 @@ const Search = ({ isLoggedIn }) => {
         }  
     }
 
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        {
+          field: 'firstName',
+          headerName: 'First name',
+          width: 150,
+          editable: true,
+        },
+        {
+          field: 'lastName',
+          headerName: 'Last name',
+          width: 150,
+          editable: true,
+        },
+        {
+          field: 'age',
+          headerName: 'Age',
+          type: 'number',
+          width: 110,
+          editable: true,
+        },
+        {
+          field: 'fullName',
+          headerName: 'Full name',
+          description: 'This column has a value getter and is not sortable.',
+          sortable: false,
+          width: 160,
+          valueGetter: (params) =>
+            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+        },
+      ];
+    
+      const rows = [
+        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+      ];
+
     
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '5%' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '5%', width: '100%' }}>
             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', width: '100%', marginBottom: '1%'}}>
                 <Button  
                     sx={{ backgroundColor: isActive.destination ? 'rgba(207, 125, 43, .31)' : 'rgba(0,0,0,0)', marginLeft: 2, marginRight: 2 }} 
@@ -209,7 +254,19 @@ const Search = ({ isLoggedIn }) => {
                         <Typography>{errorValue.message}</Typography>
                     </Box>
                 : <></> }
-                <TableContainer style={{ width: '90%', marginLeft: isLoggedIn ? '5%' : 0 }}>
+                <Box sx={{ height: 400, width: '100%' }}>
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        pageSize={5}
+                        rowsPerPageOptions={[5]}
+                        checkboxSelection
+                        disableSelectionOnClick
+                        experimentalFeatures={{ newEditingApi: true }}
+                    />
+                </Box>
+                    
+                {/* <TableContainer style={{ width: '90%', marginLeft: isLoggedIn ? '5%' : 0 }}>
                     <Table stickyHeader>
                         <TableHead>
                             <TableRow>
@@ -242,7 +299,7 @@ const Search = ({ isLoggedIn }) => {
                             })}
                         </TableBody>
                     </Table>
-                </TableContainer>
+                </TableContainer> */}
         </div>
     )
 }
