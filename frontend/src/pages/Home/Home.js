35c5d5dd47f88bbea498ebee8599f08e7fe3b22e
Kent Studer <personal@Kents-MacBook-Pro.local>
2022-11-28 17:07:42 -0600

    fixing home conditional render

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -30,7 +30,7 @@ const items = [
   }
 ]
 
-const Home = (isLoggedIn) => {
+const Home = () => {
 
   // const trips = (user.groups) ? user.groups.length : 0;
   const trips = items.length
@@ -58,7 +58,7 @@ const Home = (isLoggedIn) => {
 
   return (
       <>
-        {(isLoggedIn['isLoggedIn']) 
+        {user 
           ? 
           <>
           {(trips === 0) ?
