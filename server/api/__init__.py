c791f61579a8bec2aa14c9b3feb976279f473a47
Ankit Jain <ankitjain@mail.missouri.edu>
2022-11-28 19:57:39 -0600

    updated reqs

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -3,6 +3,8 @@ from api.config import Config
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
 from flask_mail import Mail
+from sqlalchemy import MetaData
+from sqlalchemy_schemadisplay import create_schema_graph
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,7 +17,16 @@ db.init_app(app)
 db.create_all(app=app)
 db.session.commit()
 
-
-
+# Create visual ERD
+'''
+# create the pydot graph object by autoloading all tables via a bound metadata object
+graph = create_schema_graph(metadata=MetaData(Config.SQLALCHEMY_DATABASE_URI),
+    show_datatypes=False, # The image would get nasty big if we'd show the datatypes
+    show_indexes=False, # ditto for indexes
+    rankdir='LR', # From left to right (instead of top to bottom)
+    concentrate=False # Don't try to join the relation lines together
+)
+graph.write_png('dbschema.png') # write out the file
+'''
 
     
