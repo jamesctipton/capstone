3c479d5e0d0637b47dca0e1c7168634cd7662410
Kent Studer <personal@Kents-MacBook-Pro.local>
2022-12-12 14:44:40 -0600

    image formatting for trip home and adding group settings

--- frontend/src/pages/Trip/Trip.js
+++ frontend/src/pages/Trip/Trip.js
@@ -28,34 +28,30 @@ const Trip = (props) => {
   })[0]
   console.log(group)
 
-  var samplePoll
-  
-  var totalVotes = 10
-
-    return (
-      <div>
-        <div style={{ width: '100%', marginTop: '4%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <article>
-            <img src={group.groupimage} style={{ opacity: 0.25, width: '100%', borderRadius: 20 }} />
-            <h1 style={{ fontSize: 80, color: '#CF7D30', textAlign: 'center', position: 'absolute', top: 0, bottom: 300, left: 0, right: 0, height: 'fit-content', margin: 'auto', textShadow: '#444 0px 0px 2px,   #444 0px 0px 2px,   #444 0px 0px 2px, #444 0px 0px 2px,   #444 0px 0px 2px,   #444 0px 0px 2px' }}>{group.groupname}</h1>
-            <h2 style={{ fontSize: 30, color: '#222222', textAlign: 'center', position: 'absolute', top: 0, bottom: 100, left: 0, right: 0, height: 'fit-content', margin: 'auto' }}>12th December 2023 - 24th December 2023</h2>
-          </article>
-        </div>
-        <Box sx={{marginTop: '3%'}}>
-            {(polls) ?
-            <>
-              {polls.map((poll) => (
-              <Carousel 
-                type="poll"
-                items={poll}
-                totalVotes={poll.totalVotes}
-              ></Carousel>
-            ))}
-            </>
-            : <Typography variant='h6' sx={{ width: '100%', textAlign: 'center', color: '#CF7D30' }}>You have no polls</Typography>
-          }
-        </Box>
+  return (
+    <div>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+        <article style={{  }}>
+          <img src={group.groupimage} style={{ opacity: 0.25, minWidth: '150%', height: 500, borderRadius: 20, display: 'block', overflowY: 'clip', marginLeft: '-25%' }} />
+          <h1 style={{ fontSize: 80, color: '#CF7D30', textAlign: 'center', position: 'absolute', top: 0, bottom: 400, left: 0, right: 0, height: 'fit-content', fontWeight: 400, margin: 'auto', textShadow: '#444 0px 0px 2px,   #444 0px 0px 2px,   #444 0px 0px 2px, #444 0px 0px 2px,   #444 0px 0px 2px,   #444 0px 0px 2px' }}>{group.groupname}</h1>
+          <h2 style={{ fontSize: 30, color: '#222222', textAlign: 'center', position: 'absolute', top: 0, bottom: 200, left: 0, right: 0, height: 'fit-content', margin: 'auto' }}>{group.date === '' ? group.date : 'No Date Selected Yet'}</h2>
+        </article>
       </div>
+      <Box sx={{marginTop: '3%'}}>
+          {(polls) ?
+          <>
+            {polls.map((poll) => (
+            <Carousel 
+              type="poll"
+              items={poll}
+              totalVotes={poll.totalVotes}
+            ></Carousel>
+          ))}
+          </>
+          : <Typography variant='h6' sx={{ width: '100%', textAlign: 'center', color: '#CF7D30' }}>You have no polls</Typography>
+        }
+      </Box>
+    </div>
   )};
   
   export default Trip;
